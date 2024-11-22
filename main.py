60acfddb5518b8822b7fdaef13ca9c0d296dc4ac
maksEveline <maksimeveline@gmail.com>
2024-11-23 01:17:14 +0200

    added start_func

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import os, json
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 
 def start_func():
-    pass
+    """Функция для создания необходимых директорий и файлов настроек при запуске"""
+
+    dirs = os.listdir()
+
+    if "data" not in dirs:
+        print("Creating data folder")
+        os.mkdir("data")
+
+    data_dirs = os.listdir("data")
+
+    if "settings.json" not in data_dirs:
+        print("Creating settings.json")
+        with open("data/settings.json", "w") as f:
+            data = {"API_ID": "", "API_HASH": ""}
+
+            json.dump(data, f)
 
 
 @app.route("/")
@@ -17,4 +33,5 @@ async def index():
 
 
 if __name__ == "__main__":
+    start_func()
     app.run(debug=True, port=5055)
